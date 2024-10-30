27cd7262c8835afc8e377118640806930c58b4c8
Angelos Toutsios <angelos.toutsios@gmail.com>
2024-10-30 15:18:53 -0700

    t5_implementation

---
+++ src/l2l/train/batch_train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=custom_t5_training
+#SBATCH --output=/home/angelos.toutsios.gr/data/Thesis_dev/nmt_model_train_custom/logs/log_%j.out  # Output log file
+#SBATCH --error=/home/angelos.toutsios.gr/data/Thesis_dev/nmt_model_train_custom/logs/log_%j.err   # Error log file
+#SBATCH -N 1
+#SBATCH --mem=250G
+#SBATCH --cpus-per-task=32
+#SBATCH --time=80:00:00              # Time limit (hh:mm:ss)
+#SBATCH --partition=genai            # Specify the partition
+#SBATCH --gres=gpu:1                 # Request 1 GPU
+
+# Start logging GPU usage
+watch -n 60 nvidia-smi > gpu_usage.log &  # Log every 5 seconds
+
+python -u train.py
